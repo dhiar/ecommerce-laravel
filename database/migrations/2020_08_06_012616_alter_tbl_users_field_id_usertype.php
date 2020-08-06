189b852e53c98227d111df660db1caa596540f84
dhiar <darrenzie@gmail.com>
2020-08-06 11:48:50 +0700

    alter tbl users

--- database/migrations/2020_08_06_012616_alter_tbl_users_field_id_usertype.php
+++ database/migrations/2020_08_06_012616_alter_tbl_users_field_id_usertype.php
@@ -13,17 +13,17 @@ class AlterTblUsersFieldIdUsertype extends Migration
      */
     public function up()
     {
-        //
-        // enum field gender
-        // foreign key id_user_type
-        // 
-
         Schema::table('users', function (Blueprint $table) {
             $table->unsignedBigInteger('id_user_type')->nullable(); // unsigned for foreign key.
             $table->foreign('id_user_type') // foreign key column name.
                 ->references('id') // parent table primary key.
                 ->on('user_types') // parent table name.
                 ->onDelete('set null');
+
+            $table->enum('gender', ['L', 'P'])->default('L');
+            $table->string('address',200);
+            $table->string('phone',15);
+            $table->string('photo', 100);
         });
     }
 
@@ -37,6 +37,10 @@ class AlterTblUsersFieldIdUsertype extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->dropForeign(['id_user_type']); // drop the foreign key.
             $table->dropColumn('id_user_type'); // drop the column
+            $table->dropColumn('gender');
+            $table->dropColumn('address');
+            $table->dropColumn('phone');
+            $table->dropColumn('photo');
         });
     }
 }
