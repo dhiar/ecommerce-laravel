7f304a2ae345a27816bdf5163bb7528bd02dbdff
dhiar <darrenzie@gmail.com>
2020-08-09 10:39:04 +0700

    softDeletes User & UserTypes

--- app/UserTypes.php
+++ app/UserTypes.php
@@ -4,13 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\User;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserTypes extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'user_types';
     public $timestamps = true;
     protected $guarded = ['id'];
     protected $fillable = ['name'];
+    protected $dates = ['deleted_at'];
+    
 
     public function users(){
         return $this->hasMany(User::class);
