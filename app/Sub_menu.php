88ff3e82faf2f64d4a06c17f5c79631f945ed59c
dhiar <darrenzie@gmail.com>
2020-08-09 11:04:12 +0700

    relation belongsToMany Submenu-Menu

--- app/Sub_menu.php
+++ app/Sub_menu.php
@@ -3,10 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Menu;
 
 class Sub_menu extends Model
 {
     public $timestamps = true;
     protected $guarded = ['id'];
     protected $fillable = ['id_menu','title', 'url', 'icon', 'active'];
+
+    public function menu(){
+        return $this->belongsTo(Menu::class);
+    }
 }
