349e0a89a5350b816ed090089787582725965c28
dhiar <darrenzie@gmail.com>
2020-08-13 14:15:31 +0700

    UserAdminSeeder

--- database/seeds/UserAdminSeeder.php
+++ database/seeds/UserAdminSeeder.php
@@ -16,8 +16,8 @@ class UserAdminSeeder extends Seeder
             'name'  => 'admin',
             'email' => 'darrenzie@gmail.com',
             'password' => bcrypt('manager247'),
-            'address' => 'Indonesia',
-            'phone' => '123456789',
+            'id_address' => 1,
+            'phone' => '081289482090',
             'photo' => 'https://www.travelcontinuously.com/wp-content/uploads/2018/04/empty-avatar.png'
         ]);
     }
