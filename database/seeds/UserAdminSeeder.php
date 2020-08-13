2a2b7dbb8478a4b3be5c3f43dee91d6fb3a7441c
dhiar <darrenzie@gmail.com>
2020-08-13 14:35:11 +0700

    id_user_type 3 for admin seeder

--- database/seeds/UserAdminSeeder.php
+++ database/seeds/UserAdminSeeder.php
@@ -17,6 +17,7 @@ class UserAdminSeeder extends Seeder
             'email' => 'darrenzie@gmail.com',
             'password' => bcrypt('manager247'),
             'id_address' => 1,
+            'id_user_type' => 3,
             'phone' => '081289482090',
             'photo' => 'https://www.travelcontinuously.com/wp-content/uploads/2018/04/empty-avatar.png'
         ]);
