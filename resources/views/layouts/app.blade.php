c56e4b252e69e34edd3730e63c1de64cc7ff9a5c
dhiar praditya <darrenzie@gmail.com>
2021-02-25 13:18:21 +0700

    header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,88 +3,217 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="description" content="Ecommerce Laravel">
+    <meta name="keywords" content="Fashi, unica, creative, html">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    {{-- <link href="{{ asset('bootstrap.min.css') }}" rel="stylesheet"> --}}
-
-    {{-- <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/themify-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('css/elegant-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('css/owl.carousel.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/nice-select.css') }}" rel="stylesheet">
     <link href="{{ asset('css/jquery-ui.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/slicknav.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
 </head>
 <body>
+     <!-- Page Preloder -->
+     <div id="preloder">
+        <div class="loader"></div>
+    </div>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+        <!-- Header Section Begin -->
+        <header class="header-section">
+            <div class="header-top">
+                <div class="container">
+                    <div class="ht-left">
+                        <div class="phone-service">
+                            <i class="ti-facebook"></i>
+                        </div>
+                        <div class="phone-service">
+                            <i class="ti-instagram"></i>
+                        </div>
+                        <div class="phone-service">
+                            -
+                        </div>
+                        <div class="phone-service">
+                            <i class=" fa fa-phone"></i>
+                            081289482090
+                        </div>
+                    </div>
+                    <div class="ht-right">
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
+                            <a href="{{ route('login') }}" class="login-panel"><i class="fa fa-user"></i>Login</a>
+
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                                <a href="{{ route('register') }}" class="login-panel"><i class="fa fa-registered"></i>Register &nbsp;&nbsp;&nbsp;</a>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                            <a href="{{ route('logout') }}" class="login-panel"
+                                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">    
+                            <i class="fa fa-sign-out"></i>{{ __('Logout') }}</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                            <a href="#" class="login-panel"><i class="fa fa-user-circle-o"></i>{{ Auth::user()->name }} &nbsp;&nbsp;&nbsp; </a>
                         @endguest
-                    </ul>
+                    </div>
                 </div>
             </div>
-        </nav>
+            <div class="container">
+                <div class="inner-header">
+                    <div class="row">
+                        <div class="col-lg-2 col-md-2">
+                            <div class="logo">
+                                <a href="./">
+                                    <img src="img/fashi.png" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-md-7">
+                            <div class="advanced-search" style="border: 0px !important;">
+                                <button type="button" class="category-btn">Search</button>
+                                <div class="input-group">
+                                    <input type="text" class="text-gray-dark" placeholder="What do you need?">
+                                    <button type="button"><i class="ti-search"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 text-right col-md-3">
+                            <ul class="nav-right">
+                                <li class="cart-icon">
+                                    <a href="#">
+                                        <i class="icon_bag_alt"></i>
+                                        <span>3</span>
+                                    </a>
+                                    <div class="cart-hover">
+                                        <div class="select-items">
+                                            <table>
+                                                <tbody>
+                                                    <tr>
+                                                        <td class="si-pic"><img src="img/select-product-1.jpg" alt=""></td>
+                                                        <td class="si-text">
+                                                            <div class="product-selected">
+                                                                <p>$60.00 x 1</p>
+                                                                <h6>Kabino Bedside Table</h6>
+                                                            </div>
+                                                        </td>
+                                                        <td class="si-close">
+                                                            <i class="ti-close"></i>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="si-pic"><img src="img/select-product-2.jpg" alt=""></td>
+                                                        <td class="si-text">
+                                                            <div class="product-selected">
+                                                                <p>$60.00 x 1</p>
+                                                                <h6>Kabino Bedside Table</h6>
+                                                            </div>
+                                                        </td>
+                                                        <td class="si-close">
+                                                            <i class="ti-close"></i>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="select-total">
+                                            <span>total:</span>
+                                            <h5>$120.00</h5>
+                                        </div>
+                                        <div class="select-button">
+                                            <a href="#" class="primary-btn view-card">VIEW CARD</a>
+                                            <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="cart-price">$150.00</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="nav-item">
+                <div class="container">
+                    <div class="nav-depart">
+                        <div class="depart-btn">
+                            <i class="ti-menu"></i>
+                            <span>Kategori Produk</span>
+                            <ul class="depart-hover">
+                                <li class="active"><a href="#">Women’s Clothing</a></li>
+                                <li><a href="#">Men’s Clothing</a></li>
+                                <li><a href="#">Underwear</a></li>
+                                <li><a href="#">Kid's Clothing</a></li>
+                                <li><a href="#">Brand Fashion</a></li>
+                                <li><a href="#">Accessories/Shoes</a></li>
+                                <li><a href="#">Luxury Brands</a></li>
+                                <li><a href="#">Brand Outdoor Apparel</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <nav class="nav-menu mobile-menu">
+                        <ul>
+                            <li class="active"><a href="./index.html">Home</a></li>
+                            <li><a href="./shop.html">Shop</a></li>
+                            <li><a href="#">Collection</a>
+                                <ul class="dropdown">
+                                    <li><a href="#">Men's</a></li>
+                                    <li><a href="#">Women's</a></li>
+                                    <li><a href="#">Kid's</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="./blog.html">Blog</a></li>
+                            <li><a href="./contact.html">Contact</a></li>
+                            <li><a href="#">Pages</a>
+                                <ul class="dropdown">
+                                    <li><a href="./blog-details.html">Blog Details</a></li>
+                                    <li><a href="./shopping-cart.html">Shopping Cart</a></li>
+                                    <li><a href="./check-out.html">Checkout</a></li>
+                                    <li><a href="./faq.html">Faq</a></li>
+                                    <li><a href="./register.html">Register</a></li>
+                                    <li><a href="./login.html">Login</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div id="mobile-menu-wrap"></div>
+                </div>
+            </div>
+        </header>
+        <!-- Header End -->
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+    <!-- REQUIRED SCRIPTS -->
+    <script src="/js/app.js"></script>
+
+    <!-- Js Plugins -->
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.countdown.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.zoom.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.dd.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.slicknav.js') }}"></script>
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 </body>
 </html>
